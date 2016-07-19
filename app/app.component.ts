ec3d14499df391871eaabd89f7d03f6e4363d3ef
Kamil Tekliński <kteklinski@ntrc.eu>
2016-07-19 02:00:46 +0200

    app text rewritten

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>Podstawowy szkielet Aplikacji w Angular2</h1><p>Tekla</p>'
 })
 export class AppComponent { }
 
